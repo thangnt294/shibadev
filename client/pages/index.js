86555cf440039344942dfcc662a4bff32eac58d2
thang294 <thang290495@gmail.com>
2021-11-14 22:50:43 +0700

    first commit

---
+++ client/pages/index.js
@@ -0,0 +1,10 @@
+const Index = () => {
+  return (
+      <>
+        <h1 className="jumbotron text-center bg-primary square">Online Education Marketplace</h1>
+        <p>from next js...</p>
+      </>
+  );
+};
+
+export default Index;
