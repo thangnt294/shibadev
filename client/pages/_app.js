dfad55ae079646cce8249a8b6b54860ef13db639
thang294 <thang290495@gmail.com>
2021-11-16 19:25:27 +0700

    added more codes

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,18 +1,19 @@
-import TopNav from '../components/TopNav'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'antd/dist/antd.css'
-import '../public/css/styles.css'
-import {ToastContainer} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css' 
+import TopNav from "../components/TopNav";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/antd.css";
+import "../public/css/styles.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Provider } from "../context";
 
-const MyApp = ({Component, pageProps}) => {
+const MyApp = ({ Component, pageProps }) => {
   return (
-    <>
+    <Provider>
       <ToastContainer />
       <TopNav />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
-}
+};
 
 export default MyApp;
--- client/pages/login.js
+++ client/pages/login.js
@@ -1,39 +1,60 @@
-import {useState} from 'react';
-import axios from 'axios';
-import {toast} from 'react-toastify';
-import { SyncOutlined } from '@ant-design/icons';
-import Link from 'next/link';
+import { useState, useContext } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { SyncOutlined } from "@ant-design/icons";
+import Link from "next/link";
+import { Context } from "../context/index";
 
 const Login = () => {
-  const [email, setEmail] = useState('thomas@example.com')
-  const [password, setPassword] = useState('thomas')
+  const [email, setEmail] = useState("thomas@example.com");
+  const [password, setPassword] = useState("thomas");
   const [loading, setLoading] = useState(false);
+
+  // state
+  const { state, dispatch } = useContext();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       setLoading(true);
-      const {data} = await axios.post(`/api/login`, {
+      const { data } = await axios.post(`/api/login`, {
         email,
-        password
+        password,
       });
-      console.log("LOGIN RESPONSE", data);
+      dispatch({
+        type: "LOGIN",
+        payload: data,
+      });
       // setLoading(false);
     } catch (err) {
       toast.error(err.response.data);
       setLoading(false);
     }
-  }
+  };
 
   return (
     <>
       <h1 className="jumbotron text-center bg-primary square">Login</h1>
       <div className="container col-md-4 offset-md-4 pb-5">
         <form onSubmit={handleSubmit}>
-          <input type="email" className="form-control mb-4 p-4" value={email} onChange={e => setEmail(e.target.value)} placeholder="Enter email" required />
-          <input type="password" className="form-control mb-4 p-4" value={password} onChange={e => setPassword(e.target.value)} placeholder="Enter password" required />
-          
-          <br/>
+          <input
+            type="email"
+            className="form-control mb-4 p-4"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+            required
+          />
+          <input
+            type="password"
+            className="form-control mb-4 p-4"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter password"
+            required
+          />
+
+          <br />
           <button
             type="submit"
             className="btn w-100 btn-primary"
@@ -45,11 +66,13 @@ const Login = () => {
 
         <p className="text-center p-3">
           Not yet registered?{" "}
-          <Link href="/register"><a>Register</a></Link>
+          <Link href="/register">
+            <a>Register</a>
+          </Link>
         </p>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Login;
