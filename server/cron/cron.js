eaca79531e753533621af8c46baba13a5e789a02
thangnt294 <thang290495@gmail.com>
2022-01-10 08:46:06 +0700

    added cron

--- server/cron/cron.js
+++ server/cron/cron.js
@@ -3,6 +3,7 @@ import DailyReport from "../models/dailyReport";
 import { randomNumber } from "../utils/helpers";
 
 cron.schedule("0 0 * * *", async () => {
+  console.log("CRON: Daily Report Created");
   await DailyReport.create({
     date: moment().utc().startOf("day"),
     courses: randomNumber(1, 20),
@@ -10,4 +11,8 @@ cron.schedule("0 0 * * *", async () => {
     enrollments: randomNumber(1, 30),
     profit: randomNumber(10, 50),
   });
+});
+
+cron.schedule("* * * * *", async () => {
+  console.log("TEST CRON OK");
 });
